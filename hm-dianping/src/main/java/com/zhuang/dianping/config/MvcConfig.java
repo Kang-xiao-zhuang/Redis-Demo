fad27357eb94f4b0e988f13b026c207d5236416f
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-18 12:41:40 +0800

    配置类代码

---
+++ hm-dianping/src/main/java/com/zhuang/dianping/config/MvcConfig.java
@@ -0,0 +1,40 @@
+package com.zhuang.dianping.config;
+
+import com.zhuang.dianping.utils.LoginInterceptor;
+import com.zhuang.dianping.utils.RefreshTokenInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * description: MvcConfig
+ * date: 2023/4/18 10:09
+ * author: Zhuang
+ * version: 1.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+        // token刷新的拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
+    }
+}
