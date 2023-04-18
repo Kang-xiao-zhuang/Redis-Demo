8bdb74a6649ecbc672fc8374f45d9ad1e9d12d73
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-18 12:38:12 +0800

    jedis测试类

---
+++ src/test/java/com/zhuang/redis/RedisDemoApplicationTests.java
@@ -0,0 +1,28 @@
+package com.zhuang.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    void testString() {
+        // 写入一条String数据
+        redisTemplate.opsForValue().set("name", "虎哥");
+        // 获取string数据
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
+    }
+}
