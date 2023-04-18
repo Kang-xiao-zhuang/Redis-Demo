f846b631520ceba6c2b52256f08444a0d7f1d4d8
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-18 12:36:16 +0800

    impl层

---
+++ hm-dianping/src/main/java/com/zhuang/dianping/service/impl/BlogCommentsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zhuang.dianping.service.impl;
+
+import com.zhuang.dianping.entity.BlogComments;
+import com.zhuang.dianping.mapper.BlogCommentsMapper;
+import com.zhuang.dianping.service.IBlogCommentsService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class BlogCommentsServiceImpl extends ServiceImpl<BlogCommentsMapper, BlogComments> implements IBlogCommentsService {
+
+}
---
+++ hm-dianping/src/main/java/com/zhuang/dianping/service/impl/FollowServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zhuang.dianping.service.impl;
+
+import com.zhuang.dianping.entity.Follow;
+import com.zhuang.dianping.mapper.FollowMapper;
+import com.zhuang.dianping.service.IFollowService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+
+}
