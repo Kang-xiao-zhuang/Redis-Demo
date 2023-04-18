2f01aeb6a31b3f8b3cde442a1d823004a343645b
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-18 22:04:56 +0800

    service层

--- hm-dianping/src/main/java/com/zhuang/dianping/service/IShopService.java
+++ hm-dianping/src/main/java/com/zhuang/dianping/service/IShopService.java
@@ -9,8 +9,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
  *  服务类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
+ * @author 康小庄
+ * @since 2023-4-18
  */
 public interface IShopService extends IService<Shop> {
 
--- hm-dianping/src/main/java/com/zhuang/dianping/service/IUserService.java
+++ hm-dianping/src/main/java/com/zhuang/dianping/service/IUserService.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpSession;
  *  服务类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
+ * @author 康小庄
+ * @since 2023-4-18
  */
 public interface IUserService extends IService<User> {
 
