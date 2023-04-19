35bee1db822c124ef76838e93379421354f0f0e8
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-19 22:20:11 +0800

    暴露代理对象

--- hm-dianping/src/main/java/com/zhuang/dianping/HmDianPingApplication.java
+++ hm-dianping/src/main/java/com/zhuang/dianping/HmDianPingApplication.java
@@ -3,7 +3,9 @@ package com.zhuang.dianping;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.zhuang.dianping.mapper")
 @SpringBootApplication
 public class HmDianPingApplication {
