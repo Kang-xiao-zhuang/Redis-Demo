b8f4d507ca2f3aaad997676cd75125d9fd61dc62
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-21 15:30:45 +0800

    增加测试

--- hm-dianping/src/test/java/com/zhuang/dianping/HmDianpingApplicationTests.java
+++ hm-dianping/src/test/java/com/zhuang/dianping/HmDianpingApplicationTests.java
@@ -3,12 +3,15 @@ package com.zhuang.dianping;
 import com.zhuang.dianping.service.impl.ShopServiceImpl;
 import com.zhuang.dianping.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class HmDianpingApplicationTests {
@@ -18,6 +21,9 @@ class HmDianpingApplicationTests {
 
     @Resource
     private RedisIdWorker redisIdWorker;
+
+    @Resource
+    private RedissonClient redissonClient;
 
     private ExecutorService es = Executors.newFixedThreadPool(500);
 
@@ -51,4 +57,21 @@ class HmDianpingApplicationTests {
         System.out.println("time = " + (end - begin));
     }
 
+
+    @Test
+    void testRedisson() throws Exception {
+        //获取锁(可重入)，指定锁的名称
+        RLock lock = redissonClient.getLock("anyLock");
+        //尝试获取锁，参数分别是：获取锁的最大等待时间(期间会重试)，锁自动释放时间，时间单位
+        boolean isLock = lock.tryLock(1, 10, TimeUnit.SECONDS);
+        //判断获取锁成功
+        if (isLock) {
+            try {
+                System.out.println("执行业务");
+            } finally {
+                //释放锁
+                lock.unlock();
+            }
+        }
+    }
 }
