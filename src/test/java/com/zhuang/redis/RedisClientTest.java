b6ff563b4513db3145cab2584a8256f191fd6b4a
Kang-xiao-zhuang <2247830091@qq.com>
2023-05-03 11:55:52 +0800

    RedisClient代码

---
+++ src/test/java/com/zhuang/redis/RedisClientTest.java
@@ -0,0 +1,115 @@
+package com.zhuang.redis;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RedisClientTest {
+
+    static Socket s;
+    static PrintWriter writer;
+    static BufferedReader reader;
+
+    public static final String HOST = "101.43.21.132";
+    public static final int PORT = 6379;
+
+    public static void main(String[] args) {
+        try {
+            // 1.建立连接
+            s = new Socket(HOST, PORT);
+            // 2.获取输出流、输入流
+            writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), StandardCharsets.UTF_8));
+            reader = new BufferedReader(new InputStreamReader(s.getInputStream(), StandardCharsets.UTF_8));
+
+            // 3.发出请求
+            sendRequest("ping");
+            Object obj = handleResponse();
+            System.out.println("obj = " + obj);
+
+            // 3.2.set name 虎哥
+            sendRequest("set", "name", "康小庄");
+            // 4.解析响应
+            obj = handleResponse();
+            System.out.println("obj = " + obj);
+
+            // 3.2.set name 虎哥
+            sendRequest("get", "name");
+            // 4.解析响应
+            obj = handleResponse();
+            System.out.println("obj = " + obj);
+
+            // 3.2.set name 虎哥
+            sendRequest("mget", "name", "num", "msg");
+            // 4.解析响应
+            obj = handleResponse();
+            System.out.println("obj = " + obj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 5.释放连接
+            try {
+                if (reader != null) reader.close();
+                if (writer != null) writer.close();
+                if (s != null) s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Object handleResponse() throws IOException {
+        // 读取首字节
+        int prefix = reader.read();
+        // 判断数据类型标示
+        switch (prefix) {
+            case '+': // 单行字符串，直接读一行
+                return reader.readLine();
+            case '-': // 异常，也读一行
+                throw new RuntimeException(reader.readLine());
+            case ':': // 数字
+                return Long.parseLong(reader.readLine());
+            case '$': // 多行字符串
+                // 先读长度
+                int len = Integer.parseInt(reader.readLine());
+                if (len == -1) {
+                    return null;
+                }
+                if (len == 0) {
+                    return "";
+                }
+                // 再读数据,读len个字节。我们假设没有特殊字符，所以读一行（简化）
+                return reader.readLine();
+            case '*':
+                return readBulkString();
+            default:
+                throw new RuntimeException("错误的数据格式！");
+        }
+    }
+
+    private static Object readBulkString() throws IOException {
+        // 获取数组大小
+        int len = Integer.parseInt(reader.readLine());
+        if (len <= 0) {
+            return null;
+        }
+        // 定义集合，接收多个元素
+        List<Object> list = new ArrayList<>(len);
+        // 遍历，依次读取每个元素
+        for (int i = 0; i < len; i++) {
+            list.add(handleResponse());
+        }
+        return list;
+    }
+
+    // set name 康小庄
+    private static void sendRequest(String... args) {
+        writer.println("*" + args.length);
+        for (String arg : args) {
+            writer.println("$" + arg.getBytes(StandardCharsets.UTF_8).length);
+            writer.println(arg);
+        }
+        writer.flush();
+    }
+}
